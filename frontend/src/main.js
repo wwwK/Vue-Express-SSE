437bda2b8929a04b65c1fe9051bfd80699995dbb
ysy <ysy@kipis.co.kr>
2021-07-26 16:18:03 +0900

    프론트 불필요 소스 정리

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import App from './App.vue';
-import store from './store';
 import vuetify from './plugins/vuetify';
 // vue 라우터
 import VueRouter from 'vue-router';
@@ -12,7 +11,6 @@ Vue.directive('mask', VueMaskDirective);
 
 // ::: 전역 함수 선언
 Vue.prototype.$axios = axios;
-Vue.prototype.$store = store;
 
 Vue.use(VueRouter);
 
@@ -28,22 +26,9 @@ export const router = new VueRouter({
   }
 });
 
-axios.interceptors.response.use(function (response) {
-  return response
-}, function (error) {
-  if (error.response.status === 401) {
-    localStorage.clear();
-    sessionStorage.clear();
-    router.push("/");
-    alert('로그인이 필요합니다');
-  }
-  return Promise.reject(error.response.data || error);
-})
-
 Vue.config.productionTip = false
 new Vue({
   router,
-  store,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
